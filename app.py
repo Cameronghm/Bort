f67e232c3ce9c8f60d20236bd154d51b7106f4b7
m-4ge <30839555+m-4ge@users.noreply.github.com>
2020-05-25 16:48:46 +0100

    Minimum Viable Product
    Added a class to hold all the data. Finalised the basic form: all data is now pulled and stored into an instance of the class. However, there is NO validation or sanity checking of the inputs, and the data still needs to be processed and then passed into a results page

--- app.py
+++ app.py
@@ -3,13 +3,47 @@ import sqlite3 as sq
 
 app = Flask(__name__)
 
+class data():
+    def __init__(self, savingsperyear, retirementlength, savinglength, incomegrowth,
+                 retirementcosts, investmentgrowth, currentsavings, statepension, male):
+        self.savingsperyear = savingsperyear
+        self.retirementlength = retirementlength
+        self.savinglength = savinglength
+        self.incomegrowth = incomegrowth
+        self.retirementcosts = retirementcosts
+        self.investmentgrowth = investmentgrowth
+        self.currentsavings = currentsavings
+        self.statepension = statepension
+        self.male = male
+
 @app.route('/datainput', methods=['POST', 'GET'])
 def datainput():
     if request.method == 'POST':
-        pass
+        f = request.form
+        savingsperyear = f['savingsperyear']
+        retirementlength = f['retirementlength']
+        savinglength = f['savinglength']
+        incomegrowth = f['incomegrowth']
+        retirementcosts = f['retirementcosts']
+        investmentgrowth = f['investmentgrowth']
+        currentsavings = f['currentsavings']
+        statepension = f['statepension']
+        male = f['gender']
+        print(statepension)
+        if statepension=='yes':
+            statepension = True
+        else:
+            statepension = False
+        if male=='male':
+            male = True
+        else:
+            male = False
+        data(savingsperyear, retirementlength, savinglength, incomegrowth, retirementcosts, investmentgrowth,
+             currentsavings, statepension, male)
+        return render_template("datainput.html")
 
     else:
-        return 'Hiya'
+        return render_template("datainput.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
